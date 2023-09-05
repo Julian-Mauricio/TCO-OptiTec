4bad7e0e200469abe9c236a026ea4f5af30ccc09
Julian-Mauricio <132499546+Julian-Mauricio@users.noreply.github.com>
2023-09-05 15:33:32 -0500

    05/09/2023

---
+++ src/modules/Inventario_Materia_Prima/mysql.js
@@ -0,0 +1,48 @@
+const mysql = require('mysql');
+const config = require('../../config');
+const { error } = require('../../red/respuestas');
+
+const dbConfig = {
+    host: config.mysql.host,
+    user: config.mysql.user,
+    password: config.mysql.password,
+    database: config.mysql.database,
+}
+
+let conexion;
+
+function conexionMysql() {
+    conexion = mysql.createConnection(dbConfig);
+    conexion.connect((error) => {
+        if (error) {
+            console.log('[db Error]', error);
+            setTimeout(conexionMysql, 200);
+        } else {
+            console.log('db Conectada');
+        }
+    });
+    conexion.on('error', error => {
+        console.log('[db Error]', error);
+        if (error.code === 'PROTOCOL_CONNECTION_LOST') {
+            conexionMysql();
+        } else {
+            throw error;
+        }
+    })
+}
+
+conexionMysql();
+
+function Todos(tabla) {
+    return new Promise((resolve, reject) => {
+        conexion.query(`SELECT * FROM ${tabla}`, (error, result) => {
+            if (error)
+                return reject(error);
+            resolve(result);
+        })
+    });
+};
+
+module.exports = {
+    Todos
+}
